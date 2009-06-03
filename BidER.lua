a746196b1d7e487a6209291ae1a6250a3578f462
friedmag <friedmagb@gmail.com>
2009-06-02 20:15:38 -0400

    Revamped a lot of stuff with tables.  Works a lot better treating things as dictionaries rather than arrays.  Just easier to find stuff...

--- BidER.lua
+++ BidER.lua
@@ -51,6 +51,12 @@ local function PostMsg(msg, target)
   end
 end
 
+local function count_pairs(tab)
+  local count = 0
+  for i,v in pairs(tab) do count = count + 1 end
+  return count
+end
+
 local function BidER_SendEvent(self, event, ...)
   if events[event] == nil then
     error("Invalid BidER event: " .. event)
@@ -92,14 +98,14 @@ end
 
 local function AddLink(item_link, count)
   if pick_active then
-    for i,v in ipairs(biditems) do
-      if v.item_link == item_link then
+    for i_link,v in pairs(biditems) do
+      if i_link == item_link then
         v.count = v.count + count
         Print("Updated item: " .. item_link .. "x" .. v.count)
         return
       end
     end
-    tinsert(biditems, {item_link=item_link, bids={}, count=count})
+    biditems[item_link] = {bids={}, count=count}
     Print("Adding item: " .. item_link .. "x" .. count)
   end
 end
@@ -108,8 +114,8 @@ local function EndItemPicking()
   if pick_active then
     pick_active = false
     Print("item picking completed.")
-    for i,v in ipairs(biditems) do
-      Print("Item: " .. v.item_link)
+    for item_link,v in pairs(biditems) do
+      Print("Item: " .. item_link)
     end
   else
     Print("item picking is not in progress.")
@@ -149,11 +155,12 @@ end
 local function DumpBidInfo()
   local count_msg = ""
   local player = UnitName("player")
-  for i,v in ipairs(biditems) do
-    if v.bids == nil or #v.bids == 0 then count_msg = "no bids"
-    elseif #v.bids == 1 then count_msg = #v.bids .. " bid"
-    else count_msg = #v.bids .. " bids" end
-    PostChat(v.item_link .. "x" .. v.count .. " -- " .. count_msg)
+  for item_link,v in pairs(biditems) do
+    local bid_count = count_pairs(v.bids)
+    if bid_count == 0 then count_msg = "no bids"
+    elseif bid_count == 1 then count_msg = bid_count .. " bid"
+    else count_msg = bid_count .. " bids" end
+    PostChat(item_link .. "x" .. v.count .. " -- " .. count_msg)
   end
   PostChat("To bid on any item: /w " .. player .. " <link> <bid amount>")
   PostChat("To see your current DKP: /w " .. player .. " dkp")
@@ -169,7 +176,7 @@ function events:StartAuctionCommand(args)
     return
   end
   EndItemPicking()
-  if #biditems == 0 then
+  if next(biditems) == nil then
     Print("No items have been picked for bidding.  Use /ber pick")
     return
   end
@@ -181,23 +188,25 @@ end
 
 function events:DKPCommand(args)
   local old_value
-  local name,value = strsplit(" ", args)
 
-  if dkp[name] == nil then dkp[name] = {} end
-  if dkp[name].total ~= nil then old_value = dkp[name].total end
-  dkp[name].total = tonumber(value)
-
-  if dkp[name].total == nil then
-    Print("Invalid DKP value for " .. name)
-    return
-  end
-
-  if dkp[name].total ~= old_value then
-    local was_str = ""
-    if old_value ~= nil then was_str = " (was: " .. old_value .. ")"end
-    Print("DKP value for " .. name .. " updated: " .. dkp[name].total .. was_str)
+  if args == "" then
+    for name,v in pairs(dkp) do
+      Print("DKP for " .. name .. " - " .. v.total)
+    end
   else
-    Print("DKP value for " .. name .. " unchanged (" .. old_value .. ")")
+    for name,value in args:gmatch("([A-Za-z]+)[-=:; ]([0-9]+)") do
+      if dkp[name] == nil then dkp[name] = {} end
+      if dkp[name].total ~= nil then old_value = dkp[name].total end
+      dkp[name].total = tonumber(value)
+
+      if dkp[name].total ~= old_value then
+        local was_str = ""
+        if old_value ~= nil then was_str = " (was: " .. old_value .. ")" end
+        Print("DKP value for " .. name .. " updated: " .. dkp[name].total .. was_str)
+      else
+        Print("DKP value for " .. name .. " unchanged (" .. old_value .. ")")
+      end
+    end
   end
 end
 
@@ -245,11 +254,8 @@ end
 
 local function CancelBid(who, item, bids)
   local old_value
-  for j,w in ipairs(bids) do
-    if w.name == who then
-      old_value = tremove(bids, j)
-      break
-    end
+  if bids[who] ~= nil then
+    old_value, bids[who] = bids[who], nil
   end
   if old_value == nil then
     PostMsg("You do not have a bid in for " .. item, who)
@@ -260,13 +266,10 @@ end
 
 local function PlaceBid(who, item, bids, amount)
   local tosend, old_value
-  for j,w in ipairs(bids) do
-    if w.name == who then
-      old_value = tremove(bids, j)
-      break
-    end
+  if bids[who] ~= nil then
+    old_value, bids[who] = bids[who], nil
   end
-  tinsert(bids, {name=who, amount=amount})
+  bids[who] = {amount=amount}
   if old_value then
     tosend = "Updated bid for " .. item .. " from " .. old_value.amount .. " to " .. amount
   else
@@ -288,23 +291,18 @@ function events:CHAT_MSG_WHISPER(msg, from, ...)
     end
     return
   end
-  local found
   for item, value in string.gmatch(msg, "(" .. link_regex .. ")" .. "([^|]*)") do
-    found = false
-    for i,v in ipairs(biditems) do
-      if v.item_link == item then
-        found = true
-        if value:match('cancel') then
-          CancelBid(from, item, v.bids)
-        elseif tonumber(value) ~= nil then
-          PlaceBid(from, item, v.bids, tonumber(value))
-        else
-          PostMsg("Couldn't determine bid for " .. item .. ": '" .. value .. "'", from)
-        end
-      end
-    end
-    if not found then
+    local v = biditems[item]
+    if v == nil then
       PostMsg("There is no auction in progress for " .. item, from)
+      return
+    end
+    if value:match('cancel') then
+      CancelBid(from, item, v.bids)
+    elseif tonumber(value) ~= nil then
+      PlaceBid(from, item, v.bids, tonumber(value))
+    else
+      PostMsg("Couldn't determine bid for " .. item .. ": '" .. value .. "'", from)
     end
   end
 end
